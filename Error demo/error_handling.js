d17fe42bad4ea330796fe58f69aacafdc78593fa
Sebastian Butorovic <sibutorovic@uc.cl>
2016-09-07 02:49:10 -0300

    Demo error handling ready

--- Error demo/error_handling.js
+++ Error demo/error_handling.js
@@ -1,3 +1,45 @@
 /*
 	Demo that shows how to handle errors
 */
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res,next) {
+  res.send('Hello World!');
+});
+
+app.get('/admin', function(req,res,next) {
+	var err = new Error();
+  err.status = 401;
+	err.url = req.url;
+  next(err);
+});
+
+app.get('*', function(req, res, next) {
+  var err = new Error();
+  err.status = 404;
+	err.url = req.url;
+  next(err);
+});
+
+
+// handling 404 errors
+app.use(function(err, req, res, next) {
+
+  if(err.status == 404 ) {
+    res.send("<h1>Error 404 - " + err.url + " Not found</h1>");
+  }
+	else if(err.status == 401)
+	{
+		res.write("<h1>Error 401 - Unauthorized</h1>");
+		res.write("You are not authorized to access "+ err.url);
+	}
+  else {
+		next();
+	}
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
